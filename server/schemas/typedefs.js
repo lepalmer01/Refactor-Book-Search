1eef5da96b75f2d565fa9a33307492d9076f0e13
LE Palmer <lpalmer@ucsd.edu>
2024-04-10 07:59:08 -0700

    defined typedefs

---
+++ server/schemas/typedefs.js
@@ -0,0 +1,49 @@
+const typedefs = `#graphql
+  type User {
+    _id: ID! 
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    me: User
+    test: String
+    mult(x: Int!, y: Int!): Int
+    users: [User]
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
+`
+
+module.exports = typedefs
