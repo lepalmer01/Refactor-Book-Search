d42233ea7c011a5d2f922ab47fe14e3ddecb8a8a
LE Palmer <lpalmer@ucsd.edu>
2024-04-10 07:58:09 -0700

    set up express.js server and connection to database, backend auth

--- server/server.js
+++ server/server.js
@@ -1,21 +1,55 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const {ApolloServer} = require('@apollo/server');
+const {ExpressMiddleware, expressMiddleware} = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
+const {typedefs, resolvers} = require('./schemas')
 
+// Error.stackTraceLimit = 30
+const PORT = process.env.PORT || 3505;
+const server = new ApolloServer({
+  typeDefs: typedefs,
+  resolvers,
+  plugins: [
+    {
+      requestDidStart(ctx) {
+        return {
+          didEncounterErrors(errors) {
+            console.log("************\nError\n************")
+            console.log("Query:", errors.source)
+            console.log("Errors:", errors.errors)
+          }
+        }
+      }
+    }
+  ]
+})
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+const startApolloServer = async () => {
+  await server.start()
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use('/graphql',expressMiddleware(server,{
+    context: authMiddleware
+  }))
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
-app.use(routes);
+app.get('*',(req,res) => {
+  res.sendFile(path.join(__dirname,'../client/dist/index.html'))
+})
+
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+    console.log(`use graphql sandbox at http://localhost:${PORT}/graphql`)
+  });
+
 });
+
+}
+startApolloServer()
